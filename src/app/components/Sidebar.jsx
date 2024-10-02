b88dea6418950738b9e9d8bd108f660c1ba07607
glinel <73351386+georgeslnl@users.noreply.github.com>
2024-10-02 22:42:28 +0200

    Added links to nav bar

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import Link from 'next/link';
+import { link } from 'fs';
 
 const SideNav = () => {
   const works = [
-    "Gentlewoman",
-    "Onna",
-    "Card",
-    "Noiseless Portraits",
-    "Lady Liberty",
-    "Temporary Teeth"
+    {name: "Gentlewoman", link: "/gentlewoman"},
+    {name: "Onna", link: "/onna"},
+    {name: "Card", link: "/card"},
+    {name: "Noiseless Portraits", link: "/noiseless-portraits"},
+    {name: "Lady Liberty", link: "/lady-liberty"},
+    {name: "Temporary Teeth", link: "/temporary-teeth"},
+    
+    // "Onna",
+    // "Card",
+    // "Noiseless Portraits",
+    // "Lady Liberty",
+    // "Temporary Teeth"
   ];
 
   return (
@@ -16,7 +24,9 @@ const SideNav = () => {
       <ul className="">
         {works.map((work, index) => (
           <li key={index} className="text-xs lg:text-lg hover:underline cursor-pointer">
-            {work}
+            <Link href={work.link}>
+              {work.name}
+            </Link>
           </li>
         ))}
       </ul>
