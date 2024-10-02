2717f2807328c2c2a5f37f8a929e9c891a9e1d84
glinel <73351386+georgeslnl@users.noreply.github.com>
2024-10-02 22:29:33 +0200

    Info page

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -7,12 +7,14 @@ const Header = () => {
     return (
         <header className="">
             <div className="flex justify-between items-center px-2">
-                <h1 className="text-2xl  lg:text-7xl">Maya-Aska</h1>
+                <Link href="/">
+                    <h1 className="text-2xl  lg:text-7xl">Maya-Aska</h1>
+                </Link>
                 <div className="flex space-x-4">
-                    <Link href="/info" className="text-base lg:text-2xl text-stone-700">
+                    <Link href="/info" className="text-base lg:text-2xl text-stone-700 focus:animate-ping">
                         INFO
                     </Link>
-                    <a href="https://www.instagram.com/mayaxaska/" className="text-base lg:text-2xl text-stone-700">
+                    <a href="https://www.instagram.com/mayaxaska/" className="text-base lg:text-2xl text-stone-700 focus:animate-pulse">
                         INSTAGRAM
                     </a>
                 </div>
--- src/app/info/page.jsx
+++ src/app/info/page.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import Layout from '../components/Layout';
+import { MdMailOutline } from "react-icons/md";
+import { IoDocument, IoMail } from "react-icons/io5";
 
-export default function Info() {
-    return (
-        <div>
-        <h1>Info</h1>
+
+
+const InfoPage = () => {
+  return (
+    <Layout>
+      <div className="flex flex-col w-10/12 lg:w-full max-w-4xl font-cormorant">
+        <h2 className="text-2xl lg:text-4xl mb-8">Info</h2>
+        
+        <p className="text-lg lg:text-2xl ">
+          I am Maya-Aska. I am a London based photographer. My upbringing in Paris, Tokyo and London has shaped a multi-cultural lens for me to examine the world and its traditional and contemporary values. Work experience includes: photography assistant to James Perolls including campaign for Ader Error, production for Wonderland Magazine.
+        </p>
+        
+        <div className="mt-auto lg:p-2 border-t border-black">
+          <div className="flex flex-col lg:flex-row items-center justify-between text-sm lg:text-lg">
+          <div className="flex items-center gap-1 group">
+          <IoMail className='group-hover:animate-bounce' />
+          <p> Email - mayaxaska@gmail.com</p>
+          </div>
+          <div className="flex items-center gap-1 group">
+            < IoDocument className='group-hover:animate-bounce' />
+            <p>CV upon request</p>
+            </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </Layout>
+  );
+};
+
+export default InfoPage;
