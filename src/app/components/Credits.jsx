ca2e71b45b712061121bbffc40c772cacd06af21
glinel <73351386+georgeslnl@users.noreply.github.com>
2024-10-03 13:40:00 +0200

    Added Noiseless Portraits page

--- src/app/components/Credits.jsx
+++ src/app/components/Credits.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Credits = ({ credits }) => (
-    <p className='text-xs lg:text-base px-4'>
+    <p className='text-xs lg:text-base px-2'>
       {credits.map((credit, index) => (
         <React.Fragment key={index}>
           {credit.role} - {credit.name}<br/>
---
+++ src/app/noiseless-portraits/page.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import Credits from '../components/Credits';
+import ImageGrid from '../components/ImageGrid';
+import Image from 'next/image';
+import np1 from '../images/noiseless-portraits/np-1.jpg';
+import np2 from '../images/noiseless-portraits/np-2.jpg';
+import np3 from '../images/noiseless-portraits/np-3.jpg';
+import np4 from '../images/noiseless-portraits/np-4.jpg';
+import np5 from '../images/noiseless-portraits/np-5.jpg';
+import np6 from '../images/noiseless-portraits/np-6.jpg';
+
+const imageData = [
+    { src: np1, alt: "Noiseless Portraits 1", span: 12 },
+    { src: np2, alt: "Noiseless Portraits 2", span: 12 },
+    { src: np3, alt: "Noiseless Portraits 3", span: 6 },
+    { src: np4, alt: "Noiseless Portraits 4", span: 6 },
+    { src: np5, alt: "Noiseless Portraits 5", span: 12 },
+    { src: np6, alt: "Noiseless Portraits 6", span: 12 },
+];
+
+const credits = [
+    { role: "Photography", name: "Maya-Aska" },
+    { role: "Model", name: "Joanna" },
+    { role: "Model", name: "Ellie" },
+    { role: "Stylist", name: "Isabelle Burnett" },
+    { role: "Model", name: "Lia" },
+];
+
+const NoiselessPortraitsPage = () => {
+
+    return (
+        <div className="w-11/12 lg:w-10/12 pb-10">
+            <h2 className='text-2xl lg:text-4xl mb-8'>Noiseless Portraits</h2>
+            <div>
+                <div className="grid grid-cols-12 gap-1">
+                <p className='text-xs lg:text-base col-span-12 p-2'>
+                    Photography - Maya-Aska
+                </p>
+                        <Image
+                            key={1}
+                            src={np1}
+                            alt="Noiseless Portraits 1"
+                            layout="responsive"
+                            width={500}
+                            height={300}
+                            className={`col-span-12`}
+                        />
+                <p className='text-xs lg:text-base col-span-12 p-4'>
+                    Model - Joanna
+                </p>
+                        <Image
+                            key={2}
+                            src={np2}
+                            alt="Noiseless Portraits 2"
+                            layout="responsive"
+                            width={500}
+                            height={300}
+                            className={`col-span-12`}
+                        />
+                <p className='text-xs lg:text-base col-span-12 p-4'>
+                    Model - Ellie <br />
+                    Stylist - Isabelle Burnett
+                </p>
+                        <Image
+                            key={3}
+                            src={np3}
+                            alt="Noiseless Portraits 3"
+                            layout="responsive"
+                            width={500}
+                            height={300}
+                            className={`col-span-6 row-start-6`}
+                        />
+                        <Image
+                            key={4}
+                            src={np4}
+                            alt="Noiseless Portraits 4"
+                            layout="responsive"
+                            width={500}
+                            height={300}
+                            className={`col-span-6 row-start-6`}
+                        />
+                        <p className='text-xs lg:text-base col-span-12 p-4'>
+                    Model - Lia <br />
+                    Stylist - Isabelle Burnett
+                </p>
+                        <Image
+                            key={5}
+                            src={np5}
+                            alt="Noiseless Portraits 5"
+                            layout="responsive"
+                            width={500}
+                            height={300}
+                            className={`col-span-12`}
+                        />
+                    <p className='text-xs lg:text-base col-span-12 p-4'>
+                    Model - Joanna
+                </p>
+                        <Image
+                            key={6}
+                            src={np6}
+                            alt="Noiseless Portraits 6"
+                            layout="responsive"
+                            width={500}
+                            height={300}
+                            className={`col-span-12`}
+                        />
+                <p className='text-xs lg:text-base col-span-12 p-4'>
+                    Model - Ellie <br />
+                    Stylist - Isabelle Burnett
+                </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NoiselessPortraitsPage;
