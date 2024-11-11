327af0520ce8291b0e15c6fd8017305f35a38451
glinel <73351386+georgeslnl@users.noreply.github.com>
2024-11-11 20:45:56 +0100

    Changed homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import ImageGrid from "./components/ImageGrid";
 import home1 from "./images/home/home-1.jpg";
 import home2 from "./images/home/home-2.jpg";
@@ -11,20 +10,18 @@ import home7 from "./images/home/home-7.jpg";
 import home8 from "./images/home/home-8.jpg";
 import home9 from "./images/home/home-9.jpg";
 import home10 from "./images/home/home-10.jpg";
-import home11 from "./images/home/home-11.jpg";
 
 const imageData = [
-  { src: home1, alt: "Home 1", span: 6 },
-  { src: home2, alt: "Home 2", span: 6 },
-  { src: home3, alt: "Home 3", span: 12 },
-  { src: home4, alt: "Home 4", span: 6 },
-  { src: home5, alt: "Home 5", span: 6 },
   { src: home6, alt: "Home 6", span: 6 },
   { src: home7, alt: "Home 7", span: 6 },
-  { src: home8, alt: "Home 8", span: 6 },
-  { src: home9, alt: "Home 9", span: 6 },
-  { src: home10, alt: "Home 10", span: 6 },
-  { src: home11, alt: "Home 11", span: 6 },
+  { src: home3, alt: "Home 3", span: 12 },
+  { src: home8, alt: "Home 3", span: 6 },
+  { src: home9, alt: "Home 3", span: 6 },
+  { src: home4, alt: "Home 4", span: 6 },
+  { src: home5, alt: "Home 5", span: 6 },
+  { src: home10, alt: "Home 10", span: 12 },
+  { src: home1, alt: "Home 1", span: 6 },
+  { src: home2, alt: "Home 2", span: 6 },
 ];
 
 export const metadata = {
@@ -36,7 +33,24 @@ export const metadata = {
 export default function Home() {
   return (
     <div className="w-11/12 lg:w-10/12 pb-10"> 
-    <ImageGrid images={imageData} />
+    <div className="mb-20">
+    <ImageGrid images={imageData.slice(0,2)} />
+    </div>
+    <div className="mb-20">
+    <ImageGrid images={imageData.slice(2, 3)} />
+    </div>
+    <div className="mb-20">
+    <ImageGrid images={imageData.slice(3, 5)} />
+    </div>
+    <div className="mb-20">
+    <ImageGrid images={imageData.slice(5, 7)} />
+    </div>
+    <div className="mb-20">
+    <ImageGrid images={imageData.slice(7, 8)} />
+    </div>
+    <div className="mb-20">
+    <ImageGrid images={imageData.slice(8)} />
+    </div>
     </div>
   );
 }
