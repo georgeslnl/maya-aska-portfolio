a072fa54237408e4d5d2dcc9c52716d1b3c89198
glinel <73351386+georgeslnl@users.noreply.github.com>
2024-12-10 13:02:25 +0100

    Fixed responsive layout for Works page

--- src/app/works/WorksData.jsx
+++ src/app/works/WorksData.jsx
@@ -14,20 +14,20 @@ import stillLife2 from '../images/still-life/still life 2.jpg';
 
 export const worksData = {
   '2024': [
-    { title: 'Roots', image: r1, alt: 'Root 1', href: '/roots', className: 'col-span-3 col-end-10' },
-    { title: 'Roots', image: r2, alt: 'Root 2', href: '/roots', className: 'col-span-3 col-end-13' },
-    { title: 'Still Life', image: stillLife1, alt: 'Still Life 1', href: '/still-life', className: 'col-span-4 col-end-9 row-start-2' },
-    { title: 'Still Life', image: stillLife2, alt: 'Still Life 2', href: '/still-life', className: 'col-span-4 col-end-13 row-start-2' },
-    { title: 'Onna', image: onna7, alt: 'Onna 7', href: '/onna', className: 'col-span-3 col-end-10' },
-    { title: 'Onna', image: onna10, alt: 'Onna 10', href: '/onna', className: 'col-span-3 col-end-13' },
-    { title: 'Card', image: card6, alt: 'Card 6', href: '/card', className: 'col-span-3 col-end-7' },
-    { title: 'Card', image: card4, alt: 'Card 4', href: '/card', className: 'col-span-3 col-end-10' },
-    { title: 'Card', image: card1, alt: 'Card 1', href: '/card', className: 'col-span-3 col-end-13' },
+    { title: 'Roots', image: r1, alt: 'Root 1', href: '/roots', className: 'sm:col-span-3 sm:col-end-10' },
+    { title: 'Roots', image: r2, alt: 'Root 2', href: '/roots', className: 'sm:col-span-3 sm:col-end-13' },
+    { title: 'Still Life', image: stillLife1, alt: 'Still Life 1', href: '/still-life', className: 'sm:col-span-4 sm:col-end-9 sm:row-start-2' },
+    { title: 'Still Life', image: stillLife2, alt: 'Still Life 2', href: '/still-life', className: 'sm:col-span-4 sm:col-end-13 sm:row-start-2' },
+    { title: 'Onna', image: onna7, alt: 'Onna 7', href: '/onna', className: 'sm:col-span-3 sm:col-end-10' },
+    { title: 'Onna', image: onna10, alt: 'Onna 10', href: '/onna', className: 'sm:col-span-3 sm:col-end-13' },
+    { title: 'Card', image: card6, alt: 'Card 6', href: '/card', className: 'sm:col-span-3 sm:col-end-7' },
+    { title: 'Card', image: card4, alt: 'Card 4', href: '/card', className: 'sm:col-span-3 sm:col-end-10' },
+    { title: 'Card', image: card1, alt: 'Card 1', href: '/card', className: 'sm:col-span-3 sm:col-end-13' },
   ],
   '2023': [
-    { title: 'Gentlewoman', image: gentlewoman3, alt: 'Gentlewoman 3', href: '/gentlewoman', className: 'col-span-3 col-end-10' },
-    { title: 'Gentlewoman', image: gentlewoman4, alt: 'Gentlewoman 4', href: '/gentlewoman', className: 'col-span-3 col-end-13' },
-    { title: 'Noiseless Portraits', image: np1, alt: 'Noiseless Portraits 1', href: '/noiseless-portraits', className: 'col-span-4 col-end-9' },
-    { title: 'Noiseless Portraits', image: np6, alt: 'Noiseless Portraits 6', href: '/noiseless-portraits', className: 'col-span-4 col-end-13' },
+    { title: 'Gentlewoman', image: gentlewoman3, alt: 'Gentlewoman 3', href: '/gentlewoman', className: 'sm:col-span-3 sm:col-end-10' },
+    { title: 'Gentlewoman', image: gentlewoman4, alt: 'Gentlewoman 4', href: '/gentlewoman', className: 'sm:col-span-3 sm:col-end-13' },
+    { title: 'Noiseless Portraits', image: np1, alt: 'Noiseless Portraits 1', href: '/noiseless-portraits', className: 'sm:col-span-4 sm:col-end-9' },
+    { title: 'Noiseless Portraits', image: np6, alt: 'Noiseless Portraits 6', href: '/noiseless-portraits', className: 'sm:col-span-4 sm:col-end-13' },
   ],
 };
