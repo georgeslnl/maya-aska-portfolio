5af8e6f80e6f508b9d31f5b5947a504e0b63289e
glinel <73351386+georgeslnl@users.noreply.github.com>
2025-01-04 00:52:40 +0100

    Remove Header animation

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,27 +3,20 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 
 const Header = () => {
-    const [isAnimating, setIsAnimating] = useState(false);
-
-    const handleClick = () => {
-        setIsAnimating(true);
-        setTimeout(() => setIsAnimating(false), 1000); // Animation duration
-    };
 
     return (
         <header>
             <div className="flex justify-between items-center px-2 mt-5 mb-12">
-                <Link href="/" onClick={handleClick}>
-                {/* TODO: Add typewriter animation? */}
-                    <h1 className={`cursor-pointer text-3xl lg:text-8xl p-0 ${isAnimating ? 'animate-pulse animate-fast' : ''}`}>
+                <Link href="/" >
+                    <h1 className={`cursor-pointer text-3xl lg:text-8xl p-0`}>
                         Maya-Aska
                     </h1>
                 </Link>
                 <div className="flex gap-8">
-                    <Link href="/info" className="lg:text-2xl text-base italic hover:scale-110 font-jacquarda text-stone-700">
+                    <Link href="/info" className="lg:text-2xl text-base italic hover:scale-105 font-jacquarda text-stone-700">
                         Info
                     </Link>
-                    <a href="https://www.instagram.com/mayaxaska/" className="lg:text-2xl text-base italic hover:scale-110 font-jacquarda text-stone-700">
+                    <a href="https://www.instagram.com/mayaxaska/" className="lg:text-2xl text-base italic hover:scale-105 font-jacquarda text-stone-700">
                         Instagram
                     </a>
                 </div>
