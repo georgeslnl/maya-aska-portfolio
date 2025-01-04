59d3ec81adc721be961ad5dbe9d4fbd22bac72a4
glinel <73351386+georgeslnl@users.noreply.github.com>
2025-01-04 01:37:11 +0100

    Redesign footer into sidebar

--- src/app/components/Layout.jsx
+++ src/app/components/Layout.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import Header from './Header';
 import SideNav from './Sidebar';
-import Footer from './Footer';
-
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col h-svh overflow-hidden px-4">
+    <div className="flex flex-col h-screen overflow-hidden px-4">
       <Header />
       <div className="flex flex-row flex-grow overflow-hidden">
         <SideNav />
-        <div
-          className="overflow-auto p-1 flex flex-wrap"
+        <main
+          className="flex-grow overflow-auto p-1 flex flex-wrap flex-row justify-center"
           tabIndex={1}
           role="region"
           aria-label="Main content"
         >
-          <main className="flex justify-center flex-grow">{children}</main>
-          <Footer />
-        </div>
+          {children}
+        </main>
       </div>
     </div>
   );
--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -17,43 +17,53 @@ const SideNav = () => {
   const [play] = useSound(pageTurn);
 
   return (
-    <nav 
-      className="px-2 py-1 font-cormorant min-w-fit group" 
-      role="navigation" 
-      aria-label="Works navigation"
-    >
-      <Link 
-        href="/works" 
-        className="lg:text-3xl text-lg mb-4 italic focus:outline-none"
-        aria-haspopup="true"
-        aria-expanded="true"
+    <div className="flex flex-col justify-between min-w-fit px-2 py-1 " >
+      <nav
+        className="font-cormorant group"
+        role="navigation"
+        aria-label="Works navigation"
       >
-        Works
-      </Link>
-      <ul 
-        className="opacity-0 group-focus-within:opacity-100 group-hover:opacity-100 transition-opacity duration-300"
-        role="menu"
-        aria-label="Photography works"
-      >
-        {works.map((work, index) => (
-          <li 
-            key={index} 
-            className="text-xs lg:text-lg hover:scale-105"
-            role="none"
-          >
-            <Link 
-              href={work.link}
-              onClick={play}
-              className= "focus:outline-none"
-              role="menuitem"
-              aria-label={`View ${work.name} photography series`}
+        <Link
+          href="/works"
+          className="lg:text-3xl text-lg mb-4 italic focus:outline-none"
+          aria-haspopup="true"
+          aria-expanded="true"
+        >
+          Works
+        </Link>
+        <ul
+          className="opacity-0 group-focus-within:opacity-100 group-hover:opacity-100 transition-opacity duration-300"
+          role="menu"
+          aria-label="Photography works"
+        >
+          {works.map((work, index) => (
+            <li
+              key={index}
+              className="text-xs lg:text-lg hover:scale-105"
+              role="none"
             >
-              {work.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+              <Link
+                href={work.link}
+                onClick={play}
+                className= "focus:outline-none"
+                role="menuitem"
+                aria-label={`View ${work.name} photography series`}
+              >
+                {work.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <a
+        href="https://georgeslnl.github.io/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-xs hover:scale-105 pb-1"
+      >
+        website by <span className='font-bold'>Georges</span>
+      </a>
+    </div>
   );
 };
 
