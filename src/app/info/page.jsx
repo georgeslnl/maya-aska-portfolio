710e56cfd77d207462e14cd09f9fc4febc373b8f
glinel <73351386+georgeslnl@users.noreply.github.com>
2025-01-10 22:45:17 +0100

    Fix responsive layout of Info page

--- src/app/info/page.jsx
+++ src/app/info/page.jsx
@@ -11,16 +11,16 @@ export const metadata = {
 
 const InfoPage = () => {
   return (
-      <div tabIndex={1} className="flex flex-col w-11/12 lg:w-10/12 font-cormorant">
+      <div className="flex flex-col w-11/12 lg:w-10/12">
         <h2 className="text-base lg:text-2xl mb-8">Info</h2>
         
-        <p className="text-sm lg:text-lg ">
+        <p className="text-xs lg:text-lg ">
           I am Maya-Aska. I am a London based photographer. My upbringing in Paris, Tokyo and London has shaped a multi-cultural lens for me to examine the world and its traditional and contemporary values. Work experience includes: photography assistant to James Perolls including campaign for Ader Error, production for Wonderland Magazine.
         </p>
-        <div className="m-auto lg:w-1/2"> 
+        <div className="m-auto my-10 w-3/4 lg:w-1/2"> 
         <Image src={Maya1} alt="Maya Aska"/>
         </div>
-        <div className="mt-auto lg:p-2 border-t border-black">
+        <div className=" lg:p-2 border-t border-black mt-auto">
           <div className="flex flex-col md:flex-row items-center justify-between text-xs lg:text-base">
           <div className="flex items-center gap-1 group">
           <GiDragonfly className='group-hover:animate-twBounce' />
