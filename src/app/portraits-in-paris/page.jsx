1d0ea2bae267dbc5d61fea7b47035249dbcfeefe
glinel <73351386+georgeslnl@users.noreply.github.com>
2025-01-10 14:31:43 +0100

    Rearrange image layout

--- src/app/portraits-in-paris/page.jsx
+++ src/app/portraits-in-paris/page.jsx
@@ -6,12 +6,10 @@ import pip3 from '../images/portraits-in-paris/pip3.jpg';
 import pip4 from '../images/portraits-in-paris/pip4.jpg';
 import pip5 from '../images/portraits-in-paris/pip5.jpg';
 import pip6 from '../images/portraits-in-paris/pip6.jpg';
-import pip7 from '../images/portraits-in-paris/pip7.jpg';
-import pip8 from '../images/portraits-in-paris/pip8.jpg';
-import pip9 from '../images/portraits-in-paris/pip9.jpg';
-import pip10 from '../images/portraits-in-paris/pip10.jpg';
-import pip11 from '../images/portraits-in-paris/pip11.jpg';
-import pip12 from '../images/portraits-in-paris/pip12.jpg';
+// import pip7 from '../images/portraits-in-paris/pip7.jpg';
+// import pip8 from '../images/portraits-in-paris/pip8.jpg';
+// import pip9 from '../images/portraits-in-paris/pip9.jpg';
+// import pip10 from '../images/portraits-in-paris/pip10.jpg';
 
 export const metadata = {
     title: "Portraits in Paris"
@@ -19,17 +17,15 @@ export const metadata = {
 
 const imageData = [
     { src: pip1, alt: "Portraits in Paris 1", span: 6 },
-    { src: pip3, alt: "Portraits in Paris 3", span: 6 },
-    // { src: pip2, alt: "Portraits in Paris 2", span: 12},
-    { src: pip4, alt: "Portraits in Paris 4", span: 12},
+    { src: pip2, alt: "Portraits in Paris 2", span: 6 },
+    { src: pip3, alt: "Portraits in Paris 3", span: 12},
+    { src: pip4, alt: "Portraits in Paris 4", span: 12 },
     { src: pip5, alt: "Portraits in Paris 5", span: 6 },
     { src: pip6, alt: "Portraits in Paris 6", span: 6 },
     // { src: pip7, alt: "Portraits in Paris 7", span: 6 },
-    { src: pip8, alt: "Portraits in Paris 8", span: 12 },
-    { src: pip9, alt: "Portraits in Paris 9", span: 6 },
-    { src: pip10, alt: "Portraits in Paris 10", span: 6 },
-    { src: pip11, alt: "Portraits in Paris 11", span: 6 },
-    { src: pip12, alt: "Portraits in Paris 12", span: 6 },
+    // { src: pip8, alt: "Portraits in Paris 8", span: 6 },
+    // { src: pip9, alt: "Portraits in Paris 9", span: 6 },
+    // { src: pip10, alt: "Portraits in Paris 10", span: 6 },
 ];
 
 const credits = [
@@ -43,10 +39,11 @@ const PortraitsInParisPage = () => {
     return (
         <div className="w-11/12 lg:w-10/12 pb-10">
             <h2 className='text-base lg:text-2xl mb-8'>Portraits in Paris</h2>
-            <ImageGrid images={imageData.slice(0,5)} />
+            <ImageGrid images={imageData.slice(0,2)} />
             <Credits credits={credits.slice(0,3)} />
-            <ImageGrid images={imageData.slice(5)} />
+            <ImageGrid images={imageData.slice(2, 3)} />
             <Credits credits={credits.slice(3)} />
+            <ImageGrid images={imageData.slice(3)} />
         </div>
     );
 };
