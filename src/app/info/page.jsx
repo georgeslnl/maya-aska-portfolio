1feab143eeeb915102f27644f1981f2b43b09748
glinel <73351386+georgeslnl@users.noreply.github.com>
2026-07-13 18:45:53 +0100

    Update InfoPage content

--- src/app/info/page.jsx
+++ src/app/info/page.jsx
@@ -14,7 +14,14 @@ const InfoPage = () => {
     <div className="flex flex-col w-11/12 lg:w-10/12">
       <h2 className="text-base md:text-lg lg:text-2xl mb-8">Info</h2>
       <p className="text-xs md:text-base lg:text-lg ">
-      Maya-Aska is a London based photographer. Her upbringing in Paris, Tokyo and London has shaped a multi-cultural lens for her to examine the world and its traditional and contemporary values. Commissions include: La Pyae Apothecary, Michelle Tan and Mehdi Studio.
+        Maya-Aska is a London based photographer.
+        
+      </p>
+      <p className="text-xs md:text-base lg:text-lg">
+        Her work often captures individuals within a soft, colourful world, with cinematic stillness. Her references lie in documentary photography and land artists, the latter informing her use of set design and natural elements in her images. She is particularly interested in the symbolic quality of clothing, props and environment and how they reflect her subject’s personal stories.
+      </p>
+      <p className="text-xs md:text-base lg:text-lg">
+        Maya-Aska’s commissions include EE72, Huishan Zhang, Atmos and Heroine Magazine, with work featured in Dazed, AnOther Magazine, Women’s Wear Daily, SHOWstudio and 1Granary. 
       </p>
       <div className="m-auto my-10 md:w-3/4 lg:w-1/2">
         <Image src={Maya1} alt="Maya Aska" />
